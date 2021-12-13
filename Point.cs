28d24a19de68f4c1afcaa3263e43c568efe1f177
Elena4fun <ebogushevskaya@gmail.com>
2021-12-13 21:23:44 +0300

    lesson 4 - constructors

--- Point.cs
+++ Point.cs
@@ -12,11 +12,18 @@ namespace Snake2
         public int y;
         public char sym;
 
-        public void Draw()
+        public Point(int _x, int _y, char _sym)
+        { 
+            x = _x;
+            y = _y;
+            sym = _sym;
+
+        }
+
+        public void Draw( )
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
-
 
         }
     }
--- Program.cs
+++ Program.cs
@@ -6,16 +6,10 @@ namespace Snake2
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 2;
-            p1.sym = '*';
+            Point p1 = new Point(1, 2, '*');
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 5;
-            p2.y = 8;
-            p2.sym = '*';
+            Point p2 = new Point(5, 3, '#');
             p2.Draw();
 
             Console.ReadLine ();
